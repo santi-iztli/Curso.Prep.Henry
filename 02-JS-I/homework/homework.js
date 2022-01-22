d53941745bbbb7af30ea66eb1c2d7c7b1e66a056
santimr7 <santimr7@gmail.com>
2022-01-21 19:41:08 -0600

    third commit primeras 6 preguntas resueltas

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,22 +1,31 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = hola mundo s;
+const nuevaString = hola mundo;
+var nombremascota = "zazu";
+
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
+var positivo = 27
+var negativo = -40
 
 // Crea una variable booleana:
 const nuevoBool = null;
+Meencantajavascript = true
+nomejustanlosvideojuegos = false
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
+const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * null === 40;
+const nuevaMultiplicacion = 10 * 4 === 40;
 
 // Resuelve el siguiente problema matemático:
 const nuevoModulo = 21 % 5 === null;
+const nuevoModulo = 21 % 5 === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
